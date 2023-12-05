64638340e35462419587d64b82fb133e8563ee72
Michael Boyo <boyomichaelbidemi@gmail.com>
2023-12-05 19:56:21 +0100

    hhh

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -1,3 +1,4 @@
+"use client"
 import Image from 'next/image'
 import { createElement } from 'react';
 import Link from "next/link"
@@ -5,12 +6,27 @@ import { TripsIcon } from '~/icons-tsx/TripsIcon'
 import { HomeIcon } from '~/icons-tsx/HomeIcon';
 import { SettingsIcon } from '~/icons-tsx/SettingsIcon';
 import { HIstoryIcon } from '~/icons-tsx/HIstoryIcon';
+import {usePathname} from "next/navigation"
 export const SideBar = () => {
+  const pathName = usePathname()
     return <aside className="flex flex-col h-[90vh] justify-between">
       <div className="flex flex-col">
+        {pathName}
     {navs.map((nav, i) => (
         <Link className="btn btn-ghost justify-start " href={nav.href} key={i}>
-          {createElement(nav.icon, {className: ""})}
+          {createElement(
+            "i",
+            {
+              style: {
+                stroke: new RegExp(`^(${nav.href})(/)?$`).test(
+                  pathName
+                )
+                  ? "green"
+                  : "red",
+              },
+            },
+            nav.icon
+          )}
           {nav.name}
       
       </Link>
@@ -24,8 +40,8 @@ export const SideBar = () => {
 }
 
 const navs = [
-    {name: "Home", icon: HomeIcon, href: "/"},
-    {name: "Trips", icon: TripsIcon, href: "/trips"},
-    {name: "History", icon: HIstoryIcon, href: "/trips"},
-    {name: "Settings", icon: SettingsIcon, href: "/settings"},
+    {name: "Home", icon: <HomeIcon/>, href: "/"},
+    {name: "Trips", icon: <TripsIcon/>, href: "/trips"},
+    {name: "History", icon: <HIstoryIcon/>, href: "/history"},
+    {name: "Settings", icon: <SettingsIcon/>, href: "/settings"},
 ]
