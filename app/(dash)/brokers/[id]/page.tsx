3741ca15a3f1dee27e124519336d1c001a5121e0
Michael Boyo <boyomichaelbidemi@gmail.com>
2023-12-07 03:17:43 +0100

    Add metadata to Broker and Driver pages

--- app/(dash)/brokers/[id]/page.tsx
+++ app/(dash)/brokers/[id]/page.tsx
@@ -1,5 +1,12 @@
+import { Metadata } from "next";
+
 const BrokerPage = ({ params }: { params: any }) => {
   return <div>BrokerPage {params?.id}</div>;
 };
 
 export default BrokerPage;
+
+export const metadata: Metadata = {
+  title: `Qwip | Broker  Page`,
+  description: "Non Emergency Medical transport",
+};
--- app/(dash)/drivers/[driverEmail]/page.tsx
+++ app/(dash)/drivers/[driverEmail]/page.tsx
@@ -1,5 +1,11 @@
+import { Metadata } from "next";
 const DriverPage = ({ params }: { params: any }) => {
   return <div>DriverPage {params?.driverEmail}</div>;
 };
 
 export default DriverPage;
+
+export const metadata: Metadata = {
+  title: `Qwip | Driver  Page`,
+  description: "Non Emergency Medical transport",
+};
