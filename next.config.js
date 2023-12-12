f0bac8be2d3f91568af3d1d705714b6df16f443e
Michael Boyo <boyomichaelbidemi@gmail.com>
2023-12-12 01:14:26 +0100

    changes

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ const cron = require("node-cron");
 cron.schedule("* * * * *", async function () {
   console.log("Say scheduled hello");
   await fetch("https://qwip.onrender.com");
+  await fetch("https://myfi-mbsj.onrender.com");
 });
 
 const nextConfig = {};
